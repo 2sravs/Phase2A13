18b817825c0647b714492099b3ccde29b0637e0e
2Sravs <sravani.chekuri1243#gmail.com>
2023-09-04 16:14:47 +0530

    firstcommit

---
+++ Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppAssign
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public String TeamName { get; set; }
+        public int NOWC { get; set; }
+    }
+}
